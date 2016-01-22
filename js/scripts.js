ead834776fb071c2736b8f7f625ce4c38fafa04e
Mao Thygesen <maothygesen@gmail.com>
2016-01-22 09:05:28 -0800

    add specs for return of ping

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var numberInput = function(number) {
+  return "ping";
+};
+
+
+
+  // if( numberInput% 3 === 0 ) {
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("numberInput", function() {
+  it("is going to replace number with ping", function() {
+    expect(numberInput(6)).to.equal("ping");
+  });
+});
