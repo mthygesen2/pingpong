d291710cee2446b5be6224bb171e991429d96f2c
Mao Thygesen <maothygesen@gmail.com>
2016-01-22 09:14:41 -0800

    add specs/scripts for return modulo of 15 to pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 var numberInput = function(number) {
-  if(number % 3 === 0) {
+if((number % 3 === 0) && (number % 5 === 0)) {
+  return "pingpong";
+  } else {
+    if(number % 3 === 0) {
       return "ping";
-  }else {
-    if(number % 5 === 0) {
+    }else {
+      if(number % 5 === 0) {
       return "pong";
+      }
     }
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -8,4 +8,7 @@ describe("numberInput", function() {
   it("is going to replace number with pong if the modulo of 5 equals 0", function(){
     expect(numberInput(10)).to.equal("pong");
   });
+  it("is going to replace number with pinpong if the modulo of 15 equals 0", function(){
+    expect(numberInput(30)).to.equal("pingpong");
+  });
 });
