df79d4bb2eddee30d7fc5dbe9f519aaed395f239
Mao Thygesen <maothygesen@gmail.com>
2016-01-22 11:32:22 -0800

    add specs for listing number into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
+
+//takes number and determines if it should changed
 var numberInput = function(number) {
-if((number % 3 === 0) && (number % 5 === 0)) {
-  return "pingpong";
+  if((number % 3 === 0) && (number % 5 === 0)) {
+    return "pingpong";
   } else {
     if(number % 3 === 0) {
       return "ping";
     }else {
       if(number % 5 === 0) {
-      return "pong";
+        return "pong";
       }else {
         return number;
       }
     }
   }
-  for(var i =1; i <= number; i += 1) {
-    push(number(i));
-  }
 };
+
+//makes the number into an array
+
+var numberLoop = function(loop) {
+  var resultArray = [];
+  for(var index = 1; index <= loop; index += 1) {
+    resultArray.push(numberInput(i));
+}
+};
+// var numberArray = function()
+//
 
 // $(document).ready(function(){
 //   $("#ppForm").submit(function(event) {
@@ -23,3 +33,5 @@ if((number % 3 === 0) && (number % 5 === 0)) {
 //     $(".output").text(output);
 //   });
 // });
+  // return result.push(i);
+    //
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe("numberInput", function() {
   it("is going to replace number with ping", function() {
-    expect(numberInput(6)).to.equal("ping");
+    expect(numberInput(3)).to.equal("ping");
   });
   it("is going to replace number with ping if the modulo of 3 equals 0", function(){
     expect(numberInput(9)).to.equal("ping");
@@ -14,7 +14,10 @@ describe("numberInput", function() {
   it("is going to return the number value if it is not divisible by 3 or 5", function (){
     expect(numberInput(2)).to.equal(2);
   });
+});
+
+describe("numberLoop", function() {
   it("is going to list numbers from 1 to the number input and replacing them with ping, pong or pingpong when needed", function(){
-    expect(numberInput(6)).to.equal([1,2,"ping",4,"pong",6]);
+    expect(numberInput(6)).to.equal("1,2,ping,4,pong,6");
   });
 });
